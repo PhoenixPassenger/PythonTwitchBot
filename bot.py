d61441a835b80be85d3e790bbf775447c42af18a
Rodrigo Ribeiro <rodrigo@Rodrigos-MacBook-Pro.local>
2023-12-11 09:58:40 -0300

    initial commit for python project

---
+++ bot.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+from twitchio.ext import commands
+
+# Load environment variables from .env file
+load_dotenv()
+ircToken = os.environ.get("irc_token")
+clientId = os.environ.get("client_id")
+nick = os.environ.get("nick")
+prefix = os.environ.get("prefix")
+initialChannels = os.environ.get("initial_channels")
+print(f"Initial Channels: {initialChannels}")
+
+
+bot = commands.Bot(
+    token = ircToken,
+    client_id =  clientId,
+    # nick = nick,
+    prefix = prefix,
+    initial_channels = [initialChannels]
+)
+@bot.event
+async def event_ready():
+    print('Estamos online')
+@bot. command (name='bão?')
+async def horario(ctx):
+    await ctx.channel.send(f'Oba, {ctx.author.name} bão?')
+
+bot.run()
